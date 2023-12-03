a8109b8b78a8c791eb6bd1545670698a60f1347a
lambotik <lambotik1988@gmail.com>
2023-12-03 12:31:19 +0300

    Added: Try without history

--- tests/main_page_test.py
+++ tests/main_page_test.py
@@ -10,36 +10,36 @@ class TestMainPage:
         page = MainPage(driver)
         page.open_description_page()
 
-    def test_mp_02_verify_redirection_to_telegram_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_telegram_page()
-
-    def test_mp_03_verify_redirection_to_donate_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_donate_page()
-
-    def test_mp_04_verify_redirection_to_contacts_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_contacts_page()
-
-    @pytest.mark.xfail
-    def test_mp_05_verify_redirection_to_specialists_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_specialists_page()
-        current_page_url = driver.current_url
-        assert current_page_url == MainPageLinks.URL_SPECIALISTS_PAGE, \
-            (f"The link leads to an incorrect page. \nExpected link: {MainPageLinks.URL_SPECIALISTS_PAGE}."
-             f"\nGotten link: {current_page_url}")
-
-    def test_mp_06_verify_redirection_to_github(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_github()
-
-    @pytest.mark.xfail
-    def test_mp_07_verify_redirection_to_contributors_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_contributors_page()
-
-    def test_mp_08_verify_redirection_to_used_resources_page(self, driver, main_page_open):
-        page = MainPage(driver)
-        page.open_used_resources_page()
+    # def test_mp_02_verify_redirection_to_telegram_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_telegram_page()
+    #
+    # def test_mp_03_verify_redirection_to_donate_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_donate_page()
+    #
+    # def test_mp_04_verify_redirection_to_contacts_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_contacts_page()
+    #
+    # @pytest.mark.xfail
+    # def test_mp_05_verify_redirection_to_specialists_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_specialists_page()
+    #     current_page_url = driver.current_url
+    #     assert current_page_url == MainPageLinks.URL_SPECIALISTS_PAGE, \
+    #         (f"The link leads to an incorrect page. \nExpected link: {MainPageLinks.URL_SPECIALISTS_PAGE}."
+    #          f"\nGotten link: {current_page_url}")
+    #
+    # def test_mp_06_verify_redirection_to_github(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_github()
+    #
+    # @pytest.mark.xfail
+    # def test_mp_07_verify_redirection_to_contributors_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_contributors_page()
+    #
+    # def test_mp_08_verify_redirection_to_used_resources_page(self, driver, main_page_open):
+    #     page = MainPage(driver)
+    #     page.open_used_resources_page()
