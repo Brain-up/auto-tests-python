b54ba3f7001b08a37f40e98fa932363147c22fca
QA/AQA Python Dzmitry Charukha <91555504+lambotik@users.noreply.github.com>
2023-12-06 07:54:58 +0300

    Added: (#29)
    * Added:
        Links into locator classes
    
        Refactoring:
            README.md
            To provide user with convenient copy function.
    
    * Added:
        Links into locator classes
    
        Refactoring:
            README.md
            To provide user with convenient copy function.

--- locators/locators.py
+++ locators/locators.py
@@ -2,7 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    """Locators for main page https://brainup.site/groups"""
+    """
+    Locators for main page.
+    URL: https://brainup.site
+    """
     DESCRIPTION_PAGE = (By.ID, "ember5")
     TELEGRAM_PAGE = (By.XPATH, "//*[contains(text(),'Telegram')]")
     MORE_MENU = (By.CSS_SELECTOR, "#other-menu")
@@ -16,6 +19,10 @@ class BasePageLocators:
 
 
 class LoginPageLocators:
+    """
+    Locators for login page.
+    URL:  https://brainup.site/login
+    """
     INPUT_LOGIN = (By.XPATH, '//input[@id="login"]')
     INPUT_PASSWORD = (By.XPATH, '//input[@id="password"]')
     SIGN_IN_BUTTON = (By.XPATH, '//*[@type="submit"]')
--- test_data/data.py
+++ test_data/data.py
@@ -2,6 +2,6 @@ import os
 
 
 class Data:
-    DEFAULT_USER_1 = {'login': 'default@default.ru', 'password': 'password'}
+    DEFAULT_USER_1 = {'login': 'default@default.ru', 'password': 'password'}  # default user with limited access
     LOGIN = os.getenv("LOGIN")
     PASSWORD = os.getenv("PASSWORD")
