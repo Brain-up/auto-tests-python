41a68921b78544b331438d62a490c6c6ee4346fb
lambotik <lambotik1988@gmail.com>
2023-12-13 00:58:59 +0300

    Added:     try solve task

--- tests/login_page_test.py
+++ tests/login_page_test.py
@@ -1,4 +1,8 @@
+import time
+
+from pages.autorized_user_home_page import AuthorizedUserHomePage
 from pages.login_page import LoginPage
+from pages.speech_exercises_page import SpeechExercisesPage
 from test_data.links import MainPageLinks
 
 
@@ -8,3 +12,18 @@ class TestLoginPage:
         page = LoginPage(driver)
         login_page_url = page.login_user()
         assert login_page_url == MainPageLinks.URL_GROUPS_PAGE, "The link leads to an incorrect page"
+
+
+class TestTask:
+    def test_task(self, driver, main_page_open):
+        page = LoginPage(driver)
+        page.login_user()
+
+        page = AuthorizedUserHomePage(driver)
+        page.click_speech_exercises()
+
+        page = SpeechExercisesPage(driver)
+        page.click_family_card()
+
+        time.sleep(5)
+
