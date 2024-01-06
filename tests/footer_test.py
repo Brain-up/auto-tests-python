93a4e9e1d7202e88581cf24f0c17317d1906bea5
Elena <peregovorun@gmail.com>
2024-01-06 15:19:26 +0300

    ref test_fp_01.04, update: footer_test.py, footer_page.py, base_page.py

--- tests/footer_test.py
+++ tests/footer_test.py
@@ -14,45 +14,44 @@ class TestFooter:
         locators = FooterLocators()
         urls = PagesUrls
 
-        @allure.title("Visibility of Footer on each page")
+        @allure.title("Verify presence and visibility of Footer on each page specified in the kit")
         @pytest.mark.parametrize("element_locator", locators.FOOTER_ELEMENTS_LOCATORS.values())
         @pytest.mark.parametrize("url", urls.pages_urls)
-        def test_fp_01_01_verify_visibility_of_footer_elements_on_pages(self, driver, element_locator, url):
+        def test_fp_01_01_verify_presence_and_visibility_of_footer_elements_on_pages(self, driver, element_locator, url):
             page = FooterPage(driver, url)
             page.open()
-            assert page.element_is_visible(element_locator), f"Element in Footer is invisible on the page {url}"
+            assert page.element_is_visible(element_locator), \
+                f"Element in Footer is absent or invisible on the page {url}"
 
-        @allure.title("Correctness of texts in Footer on each page")
+        @allure.title("Verify accuracy of texts in Footer on each page specified in the kit")
         @pytest.mark.parametrize("element_locator, expected_text",
                                  zip(locators.FOOTER_TEXT_LOCATORS.values(),
                                      FooterData.footer_elements_text.values())
                                  )
         @pytest.mark.parametrize("url", urls.pages_urls)
-        def test_fp_01_02_verify_text_of_elements_in_footer_on_pages(self, driver, element_locator, expected_text, url):
+        def test_fp_01_02_verify_text_in_elements_in_footer_on_pages(self, driver, element_locator, expected_text, url):
             page = FooterPage(driver, url)
             page.open()
             actual_text = page.get_text(element_locator)
-            assert actual_text in expected_text, f"The actual text '{actual_text}' of the element does not match " \
-                                                 f"any of the valid options '{' | '.join(expected_text)}' on the page {url}"
+            assert actual_text in expected_text, \
+                f"The actual text '{actual_text}' of the element does not match any of the valid options" \
+                f"'{' | '.join(expected_text)}' on the page {url}"
 
         @allure.title("Verify presence, visibility and accuracy of the image in the JETBRAINS link in Footer")
-        def test_fp_01_03_verify_image_visibility_and_accuracy_in_jetbrains_link(self, driver, main_page_open):
+        def test_fp_01_03_verify_image_in_jetbrains_link(self, driver, main_page_open):
             page = FooterPage(driver)
             assert page.check_jetbrains_image_visibility() \
                    and page.get_jetbrains_image_src() == FooterData.footer_images_src["jetbrains_img_src"] \
                    and page.get_jetbrains_image_alt() == FooterData.footer_images_alt["jetbrains_img_alt"], \
                    "The image in the JETBRAINS link is absent or invisible or inaccurate in Footer"
 
-        @allure.title("Verify visibility and accuracy of the image in the РЕГ.РУ link in Footer on the Home Page")
-        def test_fp_01_04_verify_image_visibility_and_accuracy_in_reg_link(self, driver, main_page_open):
+        @allure.title("Verify presence, visibility and accuracy of the image in the REG.RU link in Footer")
+        def test_fp_01_04_verify_image_in_reg_link(self, driver, main_page_open):
             page = FooterPage(driver)
-            reg_image = page.element_is_visible(self.locators.REG_IMAGE)
-            reg_image_src = page.get_image_src(self.locators.REG_IMAGE)
-            reg_image_alt = page.get_image_alt(self.locators.REG_IMAGE)
-            assert reg_image is not None \
-                   and reg_image_src == FooterData.footer_images_src["reg_img_src"] \
-                   and reg_image_alt == FooterData.footer_images_alt["reg_img_alt"], \
-                   f"The image in the {reg_image_alt} link is invisible or inaccurate in Footer on the Home Page"
+            assert page.check_reg_image_visibility() \
+                   and page.get_reg_image_src() == FooterData.footer_images_src["reg_img_src"] \
+                   and page.get_reg_image_alt() == FooterData.footer_images_alt["reg_img_alt"], \
+                   "The image in the REG.RU link is absent or invisible or inaccurate in Footer"
 
         @allure.title("Verify visibility and accuracy of the image in the ARASAAC link in Footer on the Home Page")
         def test_fp_01_05_verify_image_visibility_and_accuracy_in_arasaac_link(self, driver, main_page_open):
