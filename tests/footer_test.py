fb7e1246fc31f95f377419fff49e7ce038f135dd
Elena <peregovorun@gmail.com>
2024-01-08 12:05:38 +0300

    add test_fp_01.06, update footer_test.py, footer_page.py, locators.py

--- tests/footer_test.py
+++ tests/footer_test.py
@@ -61,6 +61,14 @@ class TestFooter:
                    and page.get_arasaac_image_alt() == FooterData.footer_images_alt["arasaac_img_alt"], \
                    "The image in the ARASAAC link is absent or invisible or inaccurate in Footer"
 
+        @allure.title("Verify presence, visibility and accuracy of the image in the Selectel link in Footer")
+        def test_fp_01_06_verify_image_in_selectel_link(self, driver, main_page_open):
+            page = FooterPage(driver)
+            assert page.check_selectel_image_visibility() \
+                   and page.get_selectel_image_src() == FooterData.footer_images_src["selectel_img_src"] \
+                   and page.get_selectel_image_alt() == FooterData.footer_images_alt["selectel_img_alt"], \
+                   "The image in the Selectel link is absent or invisible or inaccurate in Footer"
+
     class TestFooterForAuthorizedUserOnly:
 
         @allure.title("Verify Footer invisibility through the modal window with the exercise")
