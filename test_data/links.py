957a656c4820575fe047882ea308262c4c490414
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2024-01-08 17:59:19 +0400

    refactor: authorisation and registration tests, ci push only main

--- test_data/links.py
+++ test_data/links.py
@@ -1,18 +1,18 @@
 class MainPageLinks:
-    URL_MAIN_PAGE = "https://brainup.site"
-    # URL_MAIN_PAGE = 'https://brainup.fun'
-    URL_DESCRIPTION_PAGE = f"{URL_MAIN_PAGE}/description"
+    URL_MAIN_PAGE = "https://brainup.site/"
+    # URL_MAIN_PAGE = 'https://brainup.fun/'
+    URL_DESCRIPTION_PAGE = f"{URL_MAIN_PAGE}description"
     URL_TELEGRAM_PAGE = "https://t.me/BrainUpUsers"
     URL_DONATE_PAGE = "https://opencollective.com/brainup"
-    URL_CONTACTS = f"{URL_MAIN_PAGE}/contact"
-    URL_SPECIALISTS_PAGE = f"{URL_MAIN_PAGE}/specialists"
+    URL_CONTACTS = f"{URL_MAIN_PAGE}contact"
+    URL_SPECIALISTS_PAGE = f"{URL_MAIN_PAGE}specialists"
     URL_GITHUB = "https://github.com/Brain-up/brn"
-    URL_CONTRIBUTORS_PAGE = f"{URL_MAIN_PAGE}/contributors"
-    URL_USED_RESOURCES_PAGE = f"{URL_MAIN_PAGE}/used-resources"
-    URL_LOGIN_PAGE = f"{URL_MAIN_PAGE}/login"
-    URL_GROUPS_PAGE = f"{URL_MAIN_PAGE}/groups"
-    URL_PROFILE_PAGE = f"{URL_MAIN_PAGE}/profile"
-    URL_REGISTRATION_PAGE = f"{URL_MAIN_PAGE}/registration"
+    URL_CONTRIBUTORS_PAGE = f"{URL_MAIN_PAGE}contributors"
+    URL_USED_RESOURCES_PAGE = f"{URL_MAIN_PAGE}used-resources"
+    URL_LOGIN_PAGE = f"{URL_MAIN_PAGE}login"
+    URL_GROUPS_PAGE = f"{URL_MAIN_PAGE}groups"
+    URL_PROFILE_PAGE = f"{URL_MAIN_PAGE}profile"
+    URL_REGISTRATION_PAGE = f"{URL_MAIN_PAGE}registration"
 
 
 class SpecificExercisesUrls:
--- tests/profile_page_test.py
+++ tests/profile_page_test.py
@@ -8,9 +8,8 @@ class TestProfilePage:
     urls = MainPageLinks
 
     @allure.title("Change the password by receiving an email link")
-    def test_change_password(self, driver):
+    def test_change_password(self, driver, main_page_open):
         page = ProfilePage(driver)
-        driver.get(self.urls.URL_MAIN_PAGE)
         page.open_login_page()
         page.user_has_authorised()
         page.go_to_profile_page()
@@ -25,9 +24,8 @@ class TestProfilePage:
         assert page.get_successful_message(), "The password hasn't changed"
 
     @allure.title("Return the password by receiving an email link")
-    def test_return_password(self, driver):
+    def test_return_password(self, driver, main_page_open):
         page = ProfilePage(driver)
-        driver.get(self.urls.URL_MAIN_PAGE)
         page.open_login_page()
         page.user_has_authorised_with_new_password()
         page.go_to_profile_page()
