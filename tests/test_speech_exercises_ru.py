da115aacda6f20488d528f982b7f7213c9c3d13a
lambotik <lambotik1988@gmail.com>
2024-01-28 19:41:25 +0300

    Update: Речевые упражнения Added: test_random_cards_in_words_with_frequency_grouping_ru
    Refactoring: changed actions/checkout@v2 to actions/checkout@v3, changed group click method to select_group for any group.

--- tests/test_speech_exercises_ru.py
+++ tests/test_speech_exercises_ru.py
@@ -1,5 +1,6 @@
 import allure
 
+from locators.speech_exercises_page_locators import SpeechExercisesPageLocators
 from pages.speech_exercises_page_api import SpeechExercisesAPI
 from pages.speech_exercises_page_ru import SpeechExercisesPageRU
 
@@ -10,7 +11,7 @@ class TestCardsRU:
     @allure.title('Select random card from group "Слова" and compare UI vs BACKEND data.')
     def test_random_word_cards_ru(self, driver, specialist_user_authorized):
         page = SpeechExercisesPageRU(driver)
-        page.click_speech_exercises_words()
+        page.select_group(SpeechExercisesPageLocators.WORDS_BUTTON_RU)
         card_id = page.click_random_card_in_words()
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
 
@@ -28,7 +29,7 @@ class TestCardsRU:
     @allure.title('Select random card from group "Слова Королёвой" and compare UI vs BACKEND data.')
     def test_random_word_by_koroleva_cards_ru(self, driver, specialist_user_authorized):
         page = SpeechExercisesPageRU(driver)
-        page.click_speech_exercises_words_by_koroleva()
+        page.select_group(SpeechExercisesPageLocators.WORDS_BY_KOROLEVA_BUTTON)
         card_id = page.click_random_card_in_words_by_koroleva()
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
 
@@ -46,7 +47,7 @@ class TestCardsRU:
     @allure.title('Select a random card from "Похожие фразы" group and compare UI vs BACKEND data.')
     def test_random_cards_in_similar_phrases_ru(self, driver, specialist_user_authorized):
         page = SpeechExercisesPageRU(driver)
-        page.click_speech_exercises_similar_phrases_ru()
+        page.select_group(SpeechExercisesPageLocators.WORDS_SIMILAR_PHRASES_RU)
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
         card_id = page.click_random_card_in_similar_phrases_ru()
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
@@ -65,7 +66,7 @@ class TestCardsRU:
     @allure.title('Select a random card from "Группа слов" group and compare UI vs BACKEND data.')
     def test_random_cards_in_group_words_ru(self, driver, specialist_user_authorized):
         page = SpeechExercisesPageRU(driver)
-        page.click_speech_exercises_group_words_ru()
+        page.select_group(SpeechExercisesPageLocators.WORDS_GROUP_RU)
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
         card_id = page.click_random_card_in_group_words_ru()
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
@@ -84,7 +85,7 @@ class TestCardsRU:
     @allure.title('Select a random card from "Предложения" group and compare UI vs BACKEND data.')
     def test_random_cards_in_sentences_ru(self, driver, specialist_user_authorized):
         page = SpeechExercisesPageRU(driver)
-        page.click_speech_exercises_sentences_ru()
+        page.select_group(SpeechExercisesPageLocators.SENTENCES_RU)
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
         card_id = page.click_random_card_in_sentences_ru()
         page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
@@ -97,4 +98,23 @@ class TestCardsRU:
         id_for_back = str(driver.current_url).split('/')[-1]
         list_words_ui = page.click_start_and_get_list_words_for_group_words_ru()
         list_words_back = page.get_list_of_words_from_card_group_words(f'{id_for_back}')
+        assert sorted(list_words_ui) == sorted(list_words_back)
+
+    @allure.suite('Слова с частотной группировкой')
+    @allure.title('Select a random card from "Слова с частотной группировкой" group and compare UI vs BACKEND data.')
+    def test_random_cards_in_words_with_frequency_grouping_ru(self, driver, specialist_user_authorized):
+        page = SpeechExercisesPageRU(driver)
+        page.select_group(SpeechExercisesPageLocators.WORDS_WITH_FREQUENCY_GROUPING_RU)
+        page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
+        card_id = page.click_random_card_in_words_with_frequency_grouping_ru()
+        page.wait_changed_url(driver.current_url)  # Wait until cards will be loaded
+
+        page = SpeechExercisesAPI(driver)
+        random_id = page.get_random_id_from_list_sub_group_words_with_frequency_grouping_ru(
+            card_id)  # getting random ID from exercises group
+        driver.get(driver.current_url + f'/exercise/{random_id}')  # Open the URL with the received card ID
+        page.wait_changed_url(driver.current_url)
+        id_for_back = str(driver.current_url).split('/')[-1]
+        list_words_ui = page.click_start_and_get_list_words_ru()
+        list_words_back = page.get_list_of_words_from_card(f'{id_for_back}')
         assert sorted(list_words_ui) == sorted(list_words_back)
