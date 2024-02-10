6db016e3815b9b739e53a73cb91b769229c0fc42
Elena <peregovorun@gmail.com>
2024-02-10 18:17:53 +0300

    add test_ur_01.03, update used_resources_page_test.py, used_resources_page.py, used_resources_page_locators.py

--- locators/used_resources_page_locators.py
+++ locators/used_resources_page_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 class UsedResourcesPageLocators:
     PAGE_TITLE = (By.TAG_NAME, "h1")
     PAGE_TEXT = (By.XPATH, "//section/p")
+    LINKS_SECTION = (By.XPATH, "//section/ul")
