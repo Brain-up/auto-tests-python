25a53f4c0b845390aa71078e18d736e1d5b8f2d7
Alena Krauch <peregovorun@gmail.com>
2024-03-21 12:24:20 +0300

    add test_sp_01.03, update specialists_page_test.py, specialists_page.py, specialists_page_data.py, specialists_page_locators.py

--- locators/specialists_page_locators.py
+++ locators/specialists_page_locators.py
@@ -4,5 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 class SpecialistsPageLocators:
+    PAGE_GRID = (By.XPATH, "//div[contains(@class, 'grid')]")
+    PAGE_GRID_CONTENT = (By.XPATH, "//div[contains(@class, 'grid')]/div")
     PAGE_TEXT = (By.XPATH, "//div[contains(@class, 'text-lg')]")
     PAGE_TITLE = (By.TAG_NAME, "h2")
