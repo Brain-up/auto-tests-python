1c36d3af845a7cd39f2eb415e66820dd05fb3d03
Alena Krauch <peregovorun@gmail.com>
2024-04-10 14:26:52 +0300

    add test_su_01.03, update start_unauthorized_page_test.py, start_unauthorized_page.py, start_unauthorized_page_locators.py

--- tests/start_unauthorized_page_test.py
+++ tests/start_unauthorized_page_test.py
@@ -16,14 +16,22 @@ class TestStartUnauthorizedPage:
             assert page_content_presence is not None, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible on the page"
 
-        @allure.title("Verify structure and visibility of sections with content on the page")
-        def test_su_01_02_verify_sections_visibility(self, driver, main_page_open):
+        @allure.title("Verify amount and visibility of sections with content on the page")
+        def test_su_01_02_verify_sections_structure_and_visibility(self, driver, main_page_open):
             page = StartUnauthorizedPage(driver)
-            page_structure = page.get_amount_of_sections_on_page()
+            sections_amount = page.get_amount_of_sections_on_page()
             sections_visibility = page.check_visibility_of_sections()
-            assert page_structure == StartUnauthorizedPageData.amount_of_sections_with_content_on_page, \
+            assert sections_amount == StartUnauthorizedPageData.amount_of_sections_with_content_on_page, \
                 "The amount of sections with content does not match the expected value"
             assert sections_visibility, "Sections with content are invisible on the page"
+
+        @allure.title("Verify the composition and visibility of elements of the first level in the section 1")
+        def test_su_01_03_verify_section_1_structure_and_visibility(self, driver, main_page_open):
+            page = StartUnauthorizedPage(driver)
+            section_1_structure = page.get_structure_of_section_1()
+            elements_visibility = page.check_visibility_of_elements_in_section_1()
+            assert section_1_structure, "The section 1 is empty"
+            assert elements_visibility, "Elements in the section 1 are invisible on the page"
 
     class TestStartUnauthorizedPageText:
 
