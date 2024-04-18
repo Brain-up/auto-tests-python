67d67da05da933958d78d822d6c2eebe3a47d244
Alena Krauch <peregovorun@gmail.com>
2024-04-18 12:17:25 +0300

    add test_su_02.04, update start_unauthorized_page_test.py, start_unauthorized_page.py, start_unauthorized_page_data.py, start_unauthorized_page_locators.py

--- locators/start_unauthorized_page_locators.py
+++ locators/start_unauthorized_page_locators.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.by import By
 
 
 class StartUnauthorizedPageLocators:
-    SECTION_1_FIRST_LEVEL_ELEMENTS = (By.XPATH, '(//section)[1]/*')
-    SECTION_1_SECOND_LEVEL_ELEMENTS = (By.XPATH, '(//section)[1]/*/*')
-    SECTION_1_THIRD_LEVEL_ELEMENTS = (By.XPATH, '(//section)[1]/*/*/*')
-    SECTION_1_FOURTH_LEVEL_ELEMENTS = (By.XPATH, '(//section)[1]/*/*/*/*')
-    SECTION_2_FIRST_LEVEL_ELEMENTS = (By.XPATH, '(//section)[2]/*')
-    SECTION_2_SECOND_LEVEL_ELEMENTS = (By.XPATH, '//div[contains(@class, "m-auto")]/*')
-    SECTION_2_THIRD_LEVEL_ELEMENTS = (By.XPATH, '(//section)[2]/*//*//*')
+    SECTION_1_FIRST_LEVEL_ELEMENTS = (By.XPATH, "(//section)[1]/*")
+    SECTION_1_SECOND_LEVEL_ELEMENTS = (By.XPATH, "(//section)[1]/*/*")
+    SECTION_1_THIRD_LEVEL_ELEMENTS = (By.XPATH, "(//section)[1]/*/*/*")
+    SECTION_1_FOURTH_LEVEL_ELEMENTS = (By.XPATH, "(//section)[1]/*/*/*/*")
+    SECTION_2_FIRST_LEVEL_ELEMENTS = (By.XPATH, "(//section)[2]/*")
+    SECTION_2_SECOND_LEVEL_ELEMENTS = (By.XPATH, "//div[contains(@class, 'm-auto')]/*")
+    SECTION_2_THIRD_LEVEL_ELEMENTS = (By.XPATH, "(//section)[2]/*//*//*")
     UNAUTH_START_PAGE_CONTENT = (By.TAG_NAME, "main")
     UNAUTH_START_PAGE_SECTIONS = (By.TAG_NAME, "section")
     UNAUTH_START_PAGE_SECTION_1_TEXT = (By.XPATH, "//p[contains(@class, 'text')]")
+    UNAUTH_START_PAGE_SECTION_2_TEXT = (By.XPATH, "//div[contains(@class, 'font')]")
     UNAUTH_START_PAGE_TITLES = (By.XPATH, "//h2")
     UNAUTH_START_PAGE_SUBTITLES = (By.XPATH, "//h4")
