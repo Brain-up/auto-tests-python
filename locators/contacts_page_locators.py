4eba3cf5227f1d0de95c3f9887cbfcf4366cebbc
Alena Krauch <peregovorun@gmail.com>
2024-04-24 11:55:37 +0300

    add test_cp_01.02, add contacts_page_data.py, update contacts_page_test.py, contacts_page.py, contacts_page_locators.py, update conftest.py, links.py, header_page_locators.py

--- locators/contacts_page_locators.py
+++ locators/contacts_page_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 
 class ContactsPageLocators:
     PAGE_CONTENT = (By.TAG_NAME, "main")
+    PAGE_SECTIONS = (By.TAG_NAME, "section")
--- tests/contacts_page_test.py
+++ tests/contacts_page_test.py
@@ -1,6 +1,7 @@
 """Auto tests for verifying web elements on the 'Contacts' page"""
 import allure
 from pages.contacts_page import ContactsPage
+from test_data.contacts_page_data import ContactsPageData
 
 
 @allure.epic("The Contacts Page")
@@ -15,3 +16,12 @@ class TestContactsPage:
             page_content_visibility = page.check_visibility_of_page_content()
             assert page_content_presence is not None, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible on the page"
+
+        @allure.title("Verify amount and visibility of sections with content on the page")
+        def test_cp_01_02_verify_sections_structure_and_visibility(self, driver, contacts_page_open):
+            page = ContactsPage(driver)
+            sections_amount = page.get_amount_of_sections_on_page()
+            sections_visibility = page.check_visibility_of_sections()
+            assert sections_amount == ContactsPageData.amount_of_sections_with_content_on_page, \
+                "The amount of sections with content does not match the expected value"
+            assert sections_visibility, "Sections with content are invisible on the page"
