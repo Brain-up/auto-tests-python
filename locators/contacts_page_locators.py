7da9bf27570ba348259a7e0d8a482e1c3c8848ea
Alena Krauch <peregovorun@gmail.com>
2024-04-27 12:33:50 +0300

    ref test_cp_01.01, update contacts_page_test.py, contacts_page.py, contacts_page_locators.py

--- locators/contacts_page_locators.py
+++ locators/contacts_page_locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 
 class ContactsPageLocators:
     PAGE_CONTENT = (By.TAG_NAME, "main")
+    PAGE_STRUCTURE = (By.XPATH, "//main/*")
     PAGE_SECTIONS = (By.TAG_NAME, "section")
     SECTION_1_FIRST_LEVEL_ELEMENTS = (By.XPATH, "(//section)[1]/*")
     SECTION_2_FIRST_LEVEL_ELEMENTS = (By.XPATH, "(//section)[2]/*")
--- tests/contacts_page_test.py
+++ tests/contacts_page_test.py
@@ -14,8 +14,12 @@ class TestContactsPage:
             print(driver.current_url)
             page_content_presence = page.check_presence_of_page_content()
             page_content_visibility = page.check_visibility_of_page_content()
+            structure_of_page = page.get_structure_of_page()
+            visibility_of_elements_on_the_1st_level = page.check_visibility_of_elements_on_page()
             assert page_content_presence is not None, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible on the page"
+            assert structure_of_page, "The page is empty"
+            assert visibility_of_elements_on_the_1st_level, "1th-level elements are invisible on the page"
 
         @allure.title("Verify amount and visibility of sections with content on the page")
         def test_cp_01_02_verify_sections_structure_and_visibility(self, driver, contacts_page_open):
