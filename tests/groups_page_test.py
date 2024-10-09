2877126525f8c4f731cd8a8b04cb6d22c8661d68
Alena Krauch <peregovorun@gmail.com>
2024-10-09 12:52:46 +0300

    ref gp_test_01.02, update groups_page_test.py, groups_page.py, specialists_page_test.py

--- tests/groups_page_test.py
+++ tests/groups_page_test.py
@@ -1,6 +1,5 @@
 """Auto tests for verifying web elements on the 'Groups' page"""
 import allure
-import pytest
 from pages.groups_page import GroupsPage
 
 
@@ -16,14 +15,30 @@ class TestGroupsPage:
             assert page_content_presence is not None, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible"
 
-        @allure.title("Verify composition, visibility of elements on the 1st-2nd levels of nesting on the page")
+        @allure.title("Verify composition, visibility of elements on the 1st-4th levels of nesting on the page")
         def test_gp_01_02_verify_page_structure_and_visibility(self, driver, auto_test_user_authorized):
             page = GroupsPage(driver)
             structure_of_1st_level = page.get_structure_of_1st_level()
             visibility_of_elements_on_1st_level = page.check_elements_visibility_on_1st_level()
             structure_of_2nd_level = page.get_structure_of_2nd_level()
             visibility_of_elements_on_2nd_level = page.check_elements_visibility_on_2nd_level()
+            structure_of_3rd_level = page.get_structure_of_3rd_level()
+            visibility_of_elements_on_3rd_level = page.check_elements_visibility_on_3rd_level()
+            structure_of_4th_level = page.get_structure_of_4th_level()
+            visibility_of_elements_on_4th_level = page.check_elements_visibility_on_4th_level()
+            structure_of_5th_level = page.get_structure_of_5th_level()
+            visibility_of_elements_on_5th_level = page.check_elements_visibility_on_5th_level()
+            structure_of_6th_level = page.get_structure_of_6th_level()
+            visibility_of_elements_on_6th_level = page.check_elements_visibility_on_6th_level()
             assert structure_of_1st_level, "The page is empty"
             assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
             assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
             assert visibility_of_elements_on_2nd_level, "2nd-level elements are invisible"
+            assert structure_of_3rd_level, "Elements on the 3rd level are absent on the page"
+            assert visibility_of_elements_on_3rd_level, "3rd-level elements are invisible"
+            assert structure_of_4th_level, "Elements on the 4th level are absent on the page"
+            assert visibility_of_elements_on_4th_level, "4th-level elements are invisible"
+            assert structure_of_5th_level, "Elements on the 5th level are absent on the page"
+            assert visibility_of_elements_on_5th_level, "5th-level elements are invisible"
+            assert structure_of_6th_level, "Elements on the 6th level are absent on the page"
+            assert visibility_of_elements_on_6th_level, "6th-level elements are invisible"
