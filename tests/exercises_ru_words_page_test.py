96a03162c720c30146cc70e625e85e514a94d5d2
Alena Krauch <peregovorun@gmail.com>
2024-11-25 23:37:12 +0300

    ref test_erw_01.02, update exercises_ru_words_page_test.py, exercises_ru_words_page.py, exercises_ru_words_page_locators.py

--- tests/exercises_ru_words_page_test.py
+++ tests/exercises_ru_words_page_test.py
@@ -17,6 +17,7 @@ class TestExercisesRuWordsPage:
         @allure.title("Verify composition, visibility of elements on the 1st-6th levels of nesting on the page")
         def test_erw_01_02_verify_page_structure_and_visibility(self, driver, exercises_ru_words_page_open):
             page = ExercisesRuWordsPage(driver)
+            print(page.get_current_url())
             structure_of_1st_level = page.get_structure_of_1st_level()
             visibility_of_elements_on_1st_level = page.check_elements_visibility_on_1st_level()
             structure_of_2nd_level = page.get_structure_of_2nd_level()
@@ -29,6 +30,8 @@ class TestExercisesRuWordsPage:
             visibility_of_elements_on_5th_level = page.check_elements_visibility_on_5th_level()
             structure_of_6th_level = page.get_structure_of_6th_level()
             visibility_of_elements_on_6th_level = page.check_elements_visibility_on_6th_level()
+            structure_of_7th_level = page.get_structure_of_7th_level()
+            visibility_of_elements_on_7th_level = page.check_elements_visibility_on_7th_level()
             assert structure_of_1st_level, "The page is empty"
             assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
             assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
@@ -41,3 +44,5 @@ class TestExercisesRuWordsPage:
             assert visibility_of_elements_on_5th_level, "5th-level elements are invisible"
             assert structure_of_6th_level, "Elements on the 6th level are absent on the page"
             assert visibility_of_elements_on_6th_level, "6th-level elements are invisible"
+            assert structure_of_7th_level, "Elements on the 7th level are absent on the page"
+            assert visibility_of_elements_on_7th_level, "7th-level elements are invisible"
