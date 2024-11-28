4114cc4456903b474ac0f113b9a4434e7ddddede
Alena Krauch <peregovorun@gmail.com>
2024-11-28 23:50:18 +0300

    ref test_erw_01.03, update exercises_ru_words_page_test.py, exercises_ru_words_page.py, exercises_ru_words_page_locators.py

--- locators/exercises_ru_words_page_locators.py
+++ locators/exercises_ru_words_page_locators.py
@@ -11,3 +11,4 @@ class ExercisesRuWordsPageLocators:
     PAGE_FIFTH_LEVEL_ELEMENTS = (By.XPATH, "//main/*/*/*/*/*")
     PAGE_SIXTH_LEVEL_ELEMENTS = (By.XPATH, "//main/*/*/*/*/*/*")
     PAGE_SEVENTH_LEVEL_ELEMENTS = (By.XPATH, "//main/*/*/*/*/*/*/*")
+    PAGE_LIST1 = (By.TAG_NAME, "ul")
