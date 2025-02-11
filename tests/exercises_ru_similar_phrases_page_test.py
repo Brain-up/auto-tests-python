183d01d3335edf4ba9ad0b338f522f8f5addaf40
Alena Krauch <peregovorun@gmail.com>
2025-02-11 21:07:04 +0300

    ref test_ersp_01.02 Verify page structure and visibility, update exercises_ru_similar_phrases_page_test.py, exercises_ru_similar_phrases_page.py, exercises_ru_similar_phrases_page_locators.py

--- tests/exercises_ru_similar_phrases_page_test.py
+++ tests/exercises_ru_similar_phrases_page_test.py
@@ -19,5 +19,9 @@ class TestExercisesRuSimilarPhrasesPage:
             page = ExercisesRuSimilarPhrasesPage(driver)
             structure_of_1st_level = page.get_structure_of_1st_level()
             visibility_of_elements_on_1st_level = page.check_elements_visibility_on_1st_level()
+            structure_of_2nd_level = page.get_structure_of_2nd_level()
+            visibility_of_elements_on_2nd_level = page.check_elements_visibility_on_2nd_level()
             assert structure_of_1st_level, "The page is empty"
             assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
+            assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
+            assert visibility_of_elements_on_2nd_level, "2nd-level elements are invisible"
