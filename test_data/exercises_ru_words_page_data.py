4d3899f04c7de1f09a1ae16afb0004f5f59cb6a5
Alena Krauch <peregovorun@gmail.com>
2025-11-18 13:44:41 +0300

    update exercises_ru_words_page_test.py, del exercises_ru_words_page_data.py #433

--- test_data/exercises_ru_words_page_data.py
+++
@@ -1,8 +0,0 @@
-"""Data for verifying web elements on the 'Exercises "Words"' page on the 'ru' local"""
-from test_data.links import ExercisesUrls as ExUrls
-
-
-class ExercisesRuWordsPageData:
-    # tab_title = ("Речевые упражнения (готовы для занятий) | BrainUp", "Speech exercises | BrainUp")
-
-    breadcrumbs_urls = (ExUrls.STARTING_POINT, f"{ExUrls.STARTING_POINT}/2", ExUrls.URL_EXERCISES_RU_WORDS_PAGE)
