b0e717f52615d92e64ff09838b6c87cd1864b56f
Alena Krauch <peregovorun@gmail.com>
2025-11-27 18:23:18 +0300

    create exercises_ru_words_family_page_test.py, exercises_ru_words_family_page.py, exercises_ru_words_family_page_locators.py #623

---
+++ locators/exercises_ru_words_family_page_locators.py
@@ -0,0 +1,5 @@
+"""Locators of web elements on the 'Exercises "Family"' page in the series "Words" on the 'ru' local"""
+
+
+class ExercisesRuWordsFamilyPageLocators:
+    pass
---
+++ tests/exercises_ru_words_family_page_test.py
@@ -0,0 +1,7 @@
+"""Auto tests for verifying web elements on the 'Exercises "Family"' page in the series "Words" on the 'ru' local"""
+import allure
+
+
+@allure.epic("The Exercises 'Family' page in the series 'Words' on the 'ru' local")
+class TestExercisesRuWordsFamilyPage:
+    pass
