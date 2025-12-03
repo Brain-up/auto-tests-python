49178681c6184aaa3c8139bdb5d4c6033aa653e4
Alena Krauch <peregovorun@gmail.com>
2025-12-03 13:49:50 +0300

    ref test_erwf_01.01 Verify page presence and visibility update exercises_ru_words_family_page_test.py, exercises_ru_words_family_page.py #623

--- pages/exercises_ru_words_family_page.py
+++ pages/exercises_ru_words_family_page.py
@@ -12,3 +12,7 @@ class ExercisesRuWordsFamilyPage(BasePage):
     @allure.step("Check if some content is present in DOM")
     def check_presence_of_page_content(self):
         return self.element_is_present(self.locators.PAGE_CONTENT)
+
+    @allure.step("Check if page content is visible on the page")
+    def check_visibility_of_page_content(self):
+        return self.element_is_visible(self.locators.PAGE_CONTENT)
