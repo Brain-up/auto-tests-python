a98bb18e851085d3712e9e381c79fd617d32fe5a
Alena Krauch <peregovorun@gmail.com>
2025-12-27 13:57:48 +0300

    ref test_erwf_01.02 Verify page structure and visibility update exercises_ru_words_family_page_test.py, exercises_ru_words_family_page.py, exercises_ru_words_family_page_locators.py #623

--- tests/exercises_ru_words_family_page_test.py
+++ tests/exercises_ru_words_family_page_test.py
@@ -15,7 +15,7 @@ class TestExercisesRuWordsFamilyPage:
             assert page_content_presence, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible"
 
-        @allure.title("Verify composition, visibility of elements on the 1st-5th levels of nesting on the page")
+        @allure.title("Verify composition, visibility of elements on the 1st-6th levels of nesting on the page")
         def test_erwf_01_02_verify_page_structure_and_visibility(self, driver, exercises_ru_words_family_page_open):
             page = erwfPage(driver)
             structure_of_1st_level = page.get_structure_of_1st_level()
@@ -27,6 +27,7 @@ class TestExercisesRuWordsFamilyPage:
             structure_of_4th_level = page.get_structure_of_4th_level()
             structure_of_5th_level = page.get_structure_of_5th_level()
             visibility_of_elements_on_5th_level = page.check_elements_visibility_on_5th_level()
+            structure_of_6th_level = page.get_structure_of_6th_level()
             assert structure_of_1st_level, "The page is empty"
             assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
             assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
@@ -36,3 +37,4 @@ class TestExercisesRuWordsFamilyPage:
             assert structure_of_4th_level, "Elements on the 4th level are absent on the page"
             assert structure_of_5th_level, "Elements on the 5th level are absent on the page"
             assert visibility_of_elements_on_5th_level, "5th-level elements are invisible"
+            assert structure_of_6th_level, "Elements on the 6th level are absent on the page"
