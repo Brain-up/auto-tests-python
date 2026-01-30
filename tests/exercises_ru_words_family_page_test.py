030a8bdfc32d54aa2d43926357d93d05fc98c398
Alena Krauch <peregovorun@gmail.com>
2026-01-30 17:36:55 +0300

    ref test_erwf_01.02 Verify page structure and visibility update exercises_ru_words_family_page_test.py, exercises_ru_words_family_page.py #623

--- tests/exercises_ru_words_family_page_test.py
+++ tests/exercises_ru_words_family_page_test.py
@@ -42,6 +42,7 @@ class TestExercisesRuWordsFamilyPage:
             structure_of_12th_level = page.get_structure_of_12th_level()
             visibility_of_elements_on_12th_level = page.check_elements_visibility_on_12th_level()
             structure_of_13th_level = page.get_structure_of_13th_level()
+            visibility_of_elements_on_13th_level = page.check_elements_visibility_on_13th_level()
             assert structure_of_1st_level, "The page is empty"
             assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
             assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
@@ -66,3 +67,4 @@ class TestExercisesRuWordsFamilyPage:
             assert structure_of_12th_level, "Elements on the 12th level are absent on the page"
             assert visibility_of_elements_on_12th_level, "12th-level elements are invisible"
             assert structure_of_13th_level, "Elements on the 13th level are absent on the page"
+            assert visibility_of_elements_on_13th_level, "13th-level elements are invisible"
