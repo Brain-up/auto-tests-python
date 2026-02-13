7372c837b804d92ab005dbcfd89fdc23035a51a3
Alena Krauch <peregovorun@gmail.com>
2026-02-13 14:53:11 +0300

    ref test_erwf_01.02 Verify verify page structure and visibility add test_erwf_01.03 add test_erwf_01.04 update exercises_ru_words_family_page_test.py #623

--- tests/exercises_ru_words_family_page_test.py
+++ tests/exercises_ru_words_family_page_test.py
@@ -15,7 +15,7 @@ class TestExercisesRuWordsFamilyPage:
             assert page_content_presence, "The page content is absent in DOM"
             assert page_content_visibility, "The page content is invisible"
 
-        @allure.title("Verify composition, visibility of elements on the 1st-14th levels of nesting on the page")
+        @allure.title("Verify composition, visibility of elements on the 1st-5th levels of nesting on the page")
         def test_erwf_01_02_verify_page_structure_and_visibility(self, driver, exercises_ru_words_family_page_open):
             page = erwfPage(driver)
             structure_of_1st_level = page.get_structure_of_1st_level()
@@ -27,6 +27,19 @@ class TestExercisesRuWordsFamilyPage:
             structure_of_4th_level = page.get_structure_of_4th_level()
             structure_of_5th_level = page.get_structure_of_5th_level()
             visibility_of_elements_on_5th_level = page.check_elements_visibility_on_5th_level()
+            assert structure_of_1st_level, "The page is empty"
+            assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
+            assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
+            assert visibility_of_elements_on_2nd_level, "2nd-level elements are invisible"
+            assert structure_of_3rd_level, "Elements on the 3rd level are absent on the page"
+            assert visibility_of_elements_on_3rd_level, "3rd-level elements are invisible"
+            assert structure_of_4th_level, "Elements on the 4th level are absent on the page"
+            assert structure_of_5th_level, "Elements on the 5th level are absent on the page"
+            assert visibility_of_elements_on_5th_level, "5th-level elements are invisible"
+
+        @allure.title("Verify composition, visibility of elements on the 6st-10th levels of nesting on the page")
+        def test_erwf_01_03_verify_page_structure_and_visibility(self, driver, exercises_ru_words_family_page_open):
+            page = erwfPage(driver)
             structure_of_6th_level = page.get_structure_of_6th_level()
             visibility_of_elements_on_6th_level = page.check_elements_visibility_on_6th_level()
             structure_of_7th_level = page.get_structure_of_7th_level()
@@ -37,23 +50,6 @@ class TestExercisesRuWordsFamilyPage:
             visibility_of_elements_on_9th_level = page.check_elements_visibility_on_9th_level()
             structure_of_10th_level = page.get_structure_of_10th_level()
             visibility_of_elements_on_10th_level = page.check_elements_visibility_on_10th_level()
-            structure_of_11th_level = page.get_structure_of_11th_level()
-            visibility_of_elements_on_11th_level = page.check_elements_visibility_on_11th_level()
-            structure_of_12th_level = page.get_structure_of_12th_level()
-            visibility_of_elements_on_12th_level = page.check_elements_visibility_on_12th_level()
-            structure_of_13th_level = page.get_structure_of_13th_level()
-            visibility_of_elements_on_13th_level = page.check_elements_visibility_on_13th_level()
-            structure_of_14th_level = page.get_structure_of_14th_level()
-            visibility_of_elements_on_14th_level = page.check_elements_visibility_on_14th_level()
-            assert structure_of_1st_level, "The page is empty"
-            assert visibility_of_elements_on_1st_level, "1th-level elements are invisible"
-            assert structure_of_2nd_level, "Elements on the 2nd level are absent on the page"
-            assert visibility_of_elements_on_2nd_level, "2nd-level elements are invisible"
-            assert structure_of_3rd_level, "Elements on the 3rd level are absent on the page"
-            assert visibility_of_elements_on_3rd_level, "3rd-level elements are invisible"
-            assert structure_of_4th_level, "Elements on the 4th level are absent on the page"
-            assert structure_of_5th_level, "Elements on the 5th level are absent on the page"
-            assert visibility_of_elements_on_5th_level, "5th-level elements are invisible"
             assert structure_of_6th_level, "Elements on the 6th level are absent on the page"
             assert visibility_of_elements_on_6th_level, "6th-level elements are invisible"
             assert structure_of_7th_level, "Elements on the 7th level are absent on the page"
@@ -64,6 +60,18 @@ class TestExercisesRuWordsFamilyPage:
             assert not visibility_of_elements_on_9th_level, "9th-level elements are visible"
             assert structure_of_10th_level, "Elements on the 10th level are absent on the page"
             assert visibility_of_elements_on_10th_level, "10th-level elements are invisible"
+
+        @allure.title("Verify composition, visibility of elements on the 11st-14th levels of nesting on the page")
+        def test_erwf_01_04_verify_page_structure_and_visibility(self, driver, exercises_ru_words_family_page_open):
+            page = erwfPage(driver)
+            structure_of_11th_level = page.get_structure_of_11th_level()
+            visibility_of_elements_on_11th_level = page.check_elements_visibility_on_11th_level()
+            structure_of_12th_level = page.get_structure_of_12th_level()
+            visibility_of_elements_on_12th_level = page.check_elements_visibility_on_12th_level()
+            structure_of_13th_level = page.get_structure_of_13th_level()
+            visibility_of_elements_on_13th_level = page.check_elements_visibility_on_13th_level()
+            structure_of_14th_level = page.get_structure_of_14th_level()
+            visibility_of_elements_on_14th_level = page.check_elements_visibility_on_14th_level()
             assert structure_of_11th_level, "Elements on the 11th level are absent on the page"
             assert visibility_of_elements_on_11th_level, "11th-level elements are invisible"
             assert structure_of_12th_level, "Elements on the 12th level are absent on the page"
