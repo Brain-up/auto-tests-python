df09eb061b75379a3cc7ee789ebaeb71cf789b05
Alena Krauch <peregovorun@gmail.com>
2026-06-12 15:31:34 +0300

    ref test_hpu_03_03_verify_unauth_header_links update header_test.py #268

--- pages/header_page.py
+++ pages/header_page.py
@@ -438,14 +438,14 @@ class HeaderPage(BasePage):
     def click_more_button(self):
         return self.element_is_present_and_clickable(self.locators.MORE_BUTTON).click()
 
-    @allure.step("Click on the 'Telegram' link for unauthorizec user")
+    @allure.step("Click on the 'Telegram' link for unauthorized user")
     def click_on_Telegram_link(self):
         self.element_is_present_and_clickable(self.locators.LINK_TELEGRAM).click()
         self.driver.switch_to.window(self.driver.window_handles[1])
         return self.get_current_tab_url()
 
     @allure.step("""Click on external links in the Header and thereby open corresponding web pages on new tabs 
-    for every user""")
+    for unauthorized user""")
     def click_on_unauth_external_links_in_header(self):
         opened_pages = []
         # Click on the 'Telegram' link
